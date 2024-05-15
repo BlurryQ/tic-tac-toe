561dd6559ef86448f3a1bb0a1005901f1907d772
BlurryQ <jazz.barlow@outlook.com>
2024-05-15 20:54:31 +0100

    Added some JS

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 console.log("JS running")
 
-const Gameboard = {
-    gameboard: [[,,],[,,],[,,]],
-
-}
-
-const Players = {    
-    playerOne: {
-    name: "Player 1",
-    score: 0,
-    },
-
-    playersTwo: {
-        name: "Player 2",
-        score: 0,
+function Game () {
+    const gameboard = [[0,0,0],[0,0,0],[0,0,0]]
+    function makeChoice () {
+        const choice = prompt("Choose your position")
+        console.log(choice)
+        markPosition(choice)
+    }
+    function makePlayer (name, score, marker) {
+        player = {};
+        player.name = name;
+        player.score = score;
+        player.marker = marker
+        return player
+    }
+    function markPosition (choice) {
+        const coordinates = choice.toString()
+        console.log(coordinates)
+        const x = coordinates[1]
+        const y = coordinates[0]
+        console.log("X: " + x + " | Y: " + y)
+        gameboard[x-1][y-1] = "X"
+        console.log(gameboard)
+    }
+    function winGame () {
+        console.warn("we have a winner")
     }
 
+    const playerOne = makePlayer("Player 1", 0, "X")
+    const playerTwo = makePlayer("Player 2", 0, "O")
+    console.log("playerOne")
+    console.log(playerOne)
+    console.log("playerTwo")
+    console.log(playerTwo)
+    makeChoice()
 }
-const Game = {
-    playRound: () => {
 
-    },
-    winGame: () => {
-        console.warn("we have a winner")
-    },
-}
 
-Game.winGame()
+/* player choice:
+
+        11  12  13
+        21  22  23
+        31  32  33
+
+ */
+
+
+Game()
